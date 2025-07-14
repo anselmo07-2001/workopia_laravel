eb50bb20e6202783b460341b72c69f01db666267
Anselmo Rivera <rivera.anselmo.2001@gmail.com>
2025-07-14 16:01:34 +0800

    created the route and controller for the home controller

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
+Route::get("/", [HomeController::class, "index"]);
 Route::get("/jobs", [JobController::class, "index"]);
 Route::get("/create", [JobController::class, "create"]);
