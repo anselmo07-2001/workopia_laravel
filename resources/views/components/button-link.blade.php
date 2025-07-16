3f930062773e787cdea6ec6b9430b1aeb4929fc6
Anselmo Rivera <rivera.anselmo.2001@gmail.com>
2025-07-17 01:31:47 +0800

    implemented the mobile nav link

--- resources/views/components/button-link.blade.php
+++ resources/views/components/button-link.blade.php
@@ -1,9 +1,9 @@
 @props(["url" => "/", "icon" => null, "bgClass" => "bg-yellow-500", "hoverClass" => "hover:bg-yellow-600",
-        "textClass" => "text-black"])
+        "textClass" => "text-black", "block" => false])
 
 <a
     href="{{ $url }}"
-    class="{{ $bgClass }} {{ $hoverClass }} {{ $textClass }} px-4 py-2 rounded hover:shadow-md transition duration-300 {{ request()->is("jobs/created") ? " text-yellow-500" : "" }}"
+    class="{{ $bgClass }} {{ $hoverClass }} {{ $textClass }} {{ $block ? "block" : ""}} px-4 py-2 rounded hover:shadow-md transition duration-300 {{ request()->is("jobs/created") ? " text-yellow-500" : "" }}"
 >
     @if ($icon)
         <i class="fa fa-{{ $icon }}">
--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,8 +1,17 @@
-@props(["url" => "/", "active" => false, "icon" => null])
+@props(["url" => "/", "active" => false, "icon" => null, "mobile" => null])
 
-<a href={{ $url }} class="text-white hover:underline py-2 {{ $active ? " text-yellow-500" : "" }}">
-    @if ($icon)
-        <i class="fa fa-{{$icon}}"></i> Create Job
-    @endif
-    {{ $slot }}
-</a>
+@if ($mobile)
+     <a href="{{ $url }}" class="block px-4 py-2 hover:bg-blue-700 {{ $active ? " text-yellow-500" : "" }}">
+        @if ($icon)
+            <i class="fa fa-{{$icon}}"></i> Create Job
+        @endif
+        {{ $slot }}
+    </a>    
+@else
+    <a href="{{ $url }}" class="text-white hover:underline py-2 {{ $active ? " text-yellow-500" : "" }}">
+        @if ($icon)
+            <i class="fa fa-{{$icon}}"></i> Create Job
+        @endif
+        {{ $slot }}
+    </a>
+@endif
