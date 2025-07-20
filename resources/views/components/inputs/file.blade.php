fb8a8f6ebe60572953f06c315a5b273817b9738f
Anselmo Rivera <rivera.anselmo.2001@gmail.com>
2025-07-21 02:51:58 +0800

    Created reusable component input file, select, text area

---
+++ resources/views/components/inputs/file.blade.php
@@ -0,0 +1,23 @@
+@props(['id', "name", "label" => null, ])
+
+<div class="mb-4">
+    @if( $label )  
+        <label class="block text-gray-700" for="{{ $id }}">{{ $label }}</label>     
+    @endif
+    <input
+        id="{{ $id }}"
+        type="file"
+        name="{{ $name }}"
+        class="block w-full text-sm text-gray-900
+           border border-gray-300 rounded bg-white
+           file:mr-4 file:py-2 file:px-4
+           file:rounded file:border-0
+           file:bg-gray-200 file:text-gray-600
+           hover:file:bg-gray-300
+           focus:outline-none focus:ring-1 focus:ring-gray-400
+                @error($name) border-red-500 @enderror"
+    />
+    @error($name)
+        <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
+    @enderror
+</div>
