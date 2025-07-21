e29aefc8b8a4ec1c0f9ff6e8c049958c5c179459
Anselmo Rivera <rivera.anselmo.2001@gmail.com>
2025-07-21 15:06:16 +0800

    implemented job listings delete functionality

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -137,8 +137,15 @@ class JobController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Job $job): RedirectResponse
     {
-        //
+        // If logo, then delete it
+        if ($job->company_logo && Storage::disk('public')->exists($job->company_logo)) {
+            Storage::disk('public')->delete($job->company_logo);
+        }
+
+        $job->delete();
+
+        return redirect()->route("jobs.index")->with("success", "Job listings deleted succesfully");
     }
 }
