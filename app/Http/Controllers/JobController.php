d1937d6d0de8e45f97fa04a3a93541748d6bf26f
Anselmo Rivera <rivera.anselmo.2001@gmail.com>
2025-07-24 12:56:25 +0800

    implemented auth and guess middleware

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Job;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\View\View;
 
