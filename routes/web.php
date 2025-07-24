73f6c46846466367d449a340b92cd76cb0911d04
Anselmo Rivera <rivera.anselmo.2001@gmail.com>
2025-07-24 22:53:48 +0800

    created the dashboard controller

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\JobController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
-
+use App\Http\Controllers\DashboardController;
 
 Route::get("/", [HomeController::class, "index"])->name("home");
 // Route::resource("jobs", JobController::class);
@@ -28,3 +28,4 @@ Route::middleware("guest")->group(function() {
 });
 
 Route::post("/logout", [LoginController::class, "logout"])->name("logout");
+Route::get("/dashboard", [DashboardController::class, "index"])->name("dashboard")->middleware("auth");
