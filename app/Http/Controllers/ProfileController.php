16f0cee26a07ed99801ca7ae38235ff05be5b754
Anselmo Rivera <rivera.anselmo.2001@gmail.com>
2025-07-25 00:35:50 +0800

    implemented the update user name and email in the dashboard

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ProfileController extends Controller
+{
+    // @desc Update profile info
+    // @route PUT /profile
+    public function update(Request $request): RedirectResponse {
+        $user = Auth::user();
+
+        $validatedData = $request->validate([
+            "name" => "required|string",
+            "email" => "required|string|email"
+        ]);
+
+        $user->update($validatedData);
+
+        // Update user info
+        return redirect()->route("dashboard")->with("success", "Profile info updated!");
+    }
+}
