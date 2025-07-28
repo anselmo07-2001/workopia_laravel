ae08055b477130ffd73b558399a197c6797cd418
Anselmo Rivera <rivera.anselmo.2001@gmail.com>
2025-07-29 00:01:20 +0800

    implemented bookmarking job

--- resources/views/components/logout-button.blade.php
+++ resources/views/components/logout-button.blade.php
@@ -1,6 +1,6 @@
 <form method="POST" action="{{ route('logout') }}">
     @csrf
-    <button class="text-white" type="submit">
+    <button class="text-white cursor-pointer hover:underline hover:decoration-white" type="submit">
         <i class="fa fa-sign-out"></i> Logout
     </button>
 </form>
