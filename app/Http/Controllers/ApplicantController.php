7861a0a7cecb0ad2f7c15544c5674fba9a270c9b
Anselmo Rivera <rivera.anselmo.2001@gmail.com>
2025-07-30 03:55:03 +0800

    implemented deleting applicant and prevent multiple applications

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -12,6 +12,14 @@ class ApplicantController extends Controller
     // @desc Store new job application
     // @route POST /jobs/{job}/apply
     public function store(Request $request, Job $job): RedirectResponse {
+
+        // Check if the user has already applied
+        $existingApplicantion = Applicant::where("job_id", $job->id)->where("user_id", auth()->id())->exists();
+
+        if ($existingApplicantion) {
+            return redirect()->back()->with("error", "You have already applied to this job");
+        }
+
         // Validate incoming data
         $validatedDate = $request->validate([
             "full_name" => "required|string",
@@ -35,5 +43,13 @@ class ApplicantController extends Controller
         $application->save();
 
         return redirect()->back()->with("success", "Your application has been saved");
+    }
+
+    // @desc Delete job applicant
+    // @route DELETE /applicants/{applicant}
+    public function destroy($id) : RedirectResponse {
+        $applicant = Applicant::findOrFail($id);
+        $applicant->delete();
+        return redirect()->route("dashboard")->with("success", "Applicant delete successfully!");
     }
 }
