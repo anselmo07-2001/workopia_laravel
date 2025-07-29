561c6fc94c16a4b036f25a0fd21e65ea02f96db2
Anselmo Rivera <rivera.anselmo.2001@gmail.com>
2025-07-30 02:43:28 +0800

    implemented a feature that display application to job listing owner

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -36,7 +36,7 @@
             </h3>
 
             @forelse ($jobs as $job)
-                <div class="flex justify-between items-center border-b2-2 border-gray-200 py-2">
+                <div class="flex justify-between items-center border-b-2 border-gray-200 py-2">
                     <div>
                         <h3 class="text-xl font-semibold">{{ $job->title }}</h3>
                         <p class="text-gray-700">{{ $job->job_type }}</p>
@@ -59,8 +59,40 @@
                         </form>
                     </div>
                 </div>
+                
+                {{-- Applicants --}}
+                <div class="mt-4">
+                    <h4 class="text-lg font-semibold mb-2">Applicants</h4>
+
+                    @forelse ($job->applicants as $applicant)
+                       <div class="py-2">
+                          <p class="text-gray-800">
+                            <strong>Name: </strong> {{ $applicant->full_name }}
+                          </p> 
+                           <p class="text-gray-800">
+                            <strong>Phone: </strong> {{ $applicant->contact_phone }}
+                          </p>  
+                           <p class="text-gray-800">
+                            <strong>Email: </strong> {{ $applicant->contact_email }}
+                          </p>  
+                           <p class="text-gray-800">
+                            <strong>Message: </strong> {{ $applicant->message }}
+                          </p>
+                          <p class="text-gray-800 my-4">
+                            <a href="{{ asset('storage/' . $applicant->resume_path) }}" 
+                               class="text-blue-500 hover:underline" download>
+                                    <i class="fas fa-download"></i> Download Resume
+                            </a>
+                          </p>    
+                       </div>
+                    @empty
+                        <p class="text-gray-700">No applicants for this job</p>
+                    @endforelse
+
+                </div>
+
             @empty
-                <p class="text gray-700">You have not job listings</p>
+                <p class="text-gray-700">You have not job listings</p>
             @endforelse
         </div>
     </section>
